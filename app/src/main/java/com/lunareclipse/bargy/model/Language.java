3fbaf9dc5ea16b2adac5378d77fa8af386edae32
pkennedy89 <patrickken@gmail.com>
2018-05-23 16:57:34 +0100

    Added Language class

---
+++ app/src/main/java/com/lunareclipse/bargy/model/Language.java
@@ -0,0 +1,35 @@
+package com.lunareclipse.bargy.model;
+
+import java.io.Serializable;
+
+public class Language implements Serializable{
+
+    private String name, image, origin, type, status;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getStatus(){
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" + "Image: " + getImage() + "\n" +
+                "Origin: " + getOrigin() + "\n" + "Type: " + getType() + "\n" +
+                "Status: " + getStatus();
+    }
+}
