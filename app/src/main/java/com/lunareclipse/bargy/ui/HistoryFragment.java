40852300b6d60e3bbbdbf91960ddd5f431b10ba5
pkennedy89 <patrickken@gmail.com>
2018-06-16 13:00:36 +0100

    Finish Summary and Detail parts of HistoryFragment

--- app/src/main/java/com/lunareclipse/bargy/ui/HistoryFragment.java
+++ app/src/main/java/com/lunareclipse/bargy/ui/HistoryFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,10 +15,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lunareclipse.bargy.R;
 import com.lunareclipse.bargy.data.LanguagesAdapter;
+import com.lunareclipse.bargy.model.History;
 import com.lunareclipse.bargy.model.Language;
 import com.lunareclipse.bargy.service.LanguageClient;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,14 +49,23 @@ public class HistoryFragment extends Fragment {
     // ProgressBar for Loading
     @BindView(R.id.pb_history_loading_indicator) ProgressBar mLoadingIndicator;
 
-    // Key for Recycler Layout
-    private static final String BUNDLE_RECYCLER_LAYOUT = "HistoryFragment.recycler.layout";
+    // Views for content from FRD
+    @BindView(R.id.tv_summary) TextView mSummary;
+    @BindView(R.id.tv_detail) TextView mDetail;
+    @BindView(R.id.tv_summary_heading) TextView mSummaryHeading;
+    @BindView(R.id.tv_detail_heading) TextView mDetailHeading;
+    @BindView(R.id.tv_attribution) TextView mAttribution;
+    @BindView(R.id.v_hdivider) View mDivider;
+
+    // Firebase instance variables
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mHistoryDatabaseReference;
+    private ChildEventListener mChildEventListener;
 
     public HistoryFragment() {
         // Required empty public constructor
     }
 
-    // Inflates the RecyclerView containing the layouts for each history item
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
 
@@ -60,25 +80,57 @@ public class HistoryFragment extends Fragment {
         // Display the loading icon
         mLoadingIndicator.setVisibility(View.VISIBLE);
 
-
+        // Load the detail and description text
+        loadHistoryData();
 
         return rootView;
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //outState.putParcelable(BUNDLE_RECYCLER_LAYOUT, mRecyclerView.getLayoutManager().onSaveInstanceState());
-    }
+    private void loadHistoryData(){
+        // Initialize Firebase components
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
 
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
+        // Get a reference to the history node
+        mHistoryDatabaseReference = mFirebaseDatabase.getReference("yola/history");
 
-        if(savedInstanceState != null)
-        {
-            //Parcelable savedRecyclerLayoutState = savedInstanceState.getParcelable(BUNDLE_RECYCLER_LAYOUT);
-            //mRecyclerView.getLayoutManager().onRestoreInstanceState(savedRecyclerLayoutState);
-        }
+        // Setup the Firebase database event listener
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                History history = dataSnapshot.getValue(History.class);
+                System.out.println(history.getDetail());
+                mSummary.setText(history.getSummary());
+                mDetail.setText(history.getDetail());
+                //Hide loading icon
+                mLoadingIndicator.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        // Add the event listener
+        mHistoryDatabaseReference.addChildEventListener(mChildEventListener);
+        // Display static content
+        mSummaryHeading.setVisibility(View.VISIBLE);
+        mDivider.setVisibility(View.VISIBLE);
+        mDetailHeading.setVisibility(View.VISIBLE);
+        mAttribution.setVisibility(View.VISIBLE);
     }
 }
