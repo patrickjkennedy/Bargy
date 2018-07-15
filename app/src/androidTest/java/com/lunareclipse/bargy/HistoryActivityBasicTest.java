a37d9426566eca8ee670a35264ddd1dd532b9f9b
pkennedy89 <patrickken@gmail.com>
2018-07-15 11:21:46 +0100

    Fix RTL support issues

--- app/src/androidTest/java/com/lunareclipse/bargy/HistoryActivityBasicTest.java
+++ app/src/androidTest/java/com/lunareclipse/bargy/HistoryActivityBasicTest.java
@@ -12,7 +12,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class HistoryActivityBasicTest {
