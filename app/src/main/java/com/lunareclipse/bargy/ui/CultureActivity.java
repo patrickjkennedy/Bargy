701021a5f1bbd8c671a5438e6c043e5844a2d5bc
pkennedy89 <patrickken@gmail.com>
2018-07-16 18:58:11 +0100

    App theme extends AppCompat. App uses and app bar and associated toolbars.

--- app/src/main/java/com/lunareclipse/bargy/ui/CultureActivity.java
+++ app/src/main/java/com/lunareclipse/bargy/ui/CultureActivity.java
@@ -1,8 +1,9 @@
 package com.lunareclipse.bargy.ui;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
 import com.lunareclipse.bargy.R;
 
 public class CultureActivity extends AppCompatActivity {
@@ -11,5 +12,15 @@ public class CultureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_culture);
+
+        // Set the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/lunareclipse/bargy/ui/HomeActivity.java
+++ app/src/main/java/com/lunareclipse/bargy/ui/HomeActivity.java
@@ -2,8 +2,8 @@ package com.lunareclipse.bargy.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.lunareclipse.bargy.R;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +11,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Set the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
